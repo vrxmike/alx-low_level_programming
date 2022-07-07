0b983b660084c788c0a68418cc255a1694051bea
vrxmike <alvinvrxmike@icloud.com>
2022-07-07 03:55:26 -0700

    printing the last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,13 @@
 * @n: the passed args
 * Return: the last digit
 */
-int print_last digit(int n)
+int print_last_digit(int n)
 {
 	int x;
 
-	if (n < '0')
+	if (n < 0)
 		n = -n;
 	x = n % '10';
-	_putchar('x' + '0');
+	_putchar(x + '0');
 	return (x);
 }
