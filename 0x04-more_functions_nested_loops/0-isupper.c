9788e0b88ec0209f8ddc07416b2cd5b14d880be3
vrxmike <alvinvrxmike@icloud.com>
2022-07-08 06:32:32 -0700

    0-Task check isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
-  * _isupper - checks if a letter is upper case
-  *  @c: print int
-  *  Return: 1 for upper letter or 0 for any else
-  */
+ * _isupper - checks upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-	return (0);
-	}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
 }
