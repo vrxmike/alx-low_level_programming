b941e4e3f6ac552cd32909879bcca28b1edde000
vrxmike <alvinvrxmike@icloud.com>
2022-07-08 04:25:53 -0700

    9-Task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * fizz_buzz -> printing to 100
+  */
+
+void fizz_buzz(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x == 100)
+			printf("Buzz ");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", x);
+	}
+	printf("\n");
+}
