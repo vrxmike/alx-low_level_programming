9d7711a0c9b7d0b906d7ea32a0fb99b42356c67c
vrxmike <alvinvrxmike@icloud.com>
2022-07-08 16:43:05 -0700

    Task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
