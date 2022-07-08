3d90267e3c6ad50d02f80bb16b0c01c6e92ed2a0
vrxmike <alvinvrxmike@icloud.com>
2022-07-08 03:38:57 -0700

    hot fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 		_putchar('\n');
 	else
 	{
-		for (x =0; x < size; x++)
+		for (x = 0; x < size; x++)
 		{
-			for (y =0; y < size; y++)
+			for (y = 0; y < size; y++)
 			{
 				_putchar(35);
 			}
