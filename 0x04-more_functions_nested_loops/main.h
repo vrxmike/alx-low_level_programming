fb0012894208e5309efecb438a6b6f3ee8e70f31
vrxmike <alvinvrxmike@icloud.com>
2022-07-08 08:21:54 -0700

    0-Task pushed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,6 +10,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+int _putchar(char c);
 void fizz_buzz(void)
 void print_triangle(int size);
 void print_number(int n);
