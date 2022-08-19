4c0f351b6dc5b83723709ac81fe8f01d5967d963
vrxmike <alvinvrxmike@ivloud.com>
2022-08-19 04:00:02 -0700

    4-Task hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
-int clear_bit(unsigned lont int *n, unsigned nt index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int set;
 
