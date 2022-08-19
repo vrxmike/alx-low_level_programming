30f3eb46999d67f793adf1c5634fa2120e9b1941
vrxmike <alvinvrxmike@ivloud.com>
2022-08-19 03:07:36 -0700

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (i = 0; b[i] != '\0' ;i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		num <<= 1;
 		if (b[i] == '1')
