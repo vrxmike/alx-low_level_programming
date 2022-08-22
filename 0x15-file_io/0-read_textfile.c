24d8cf4a535cdd0267bbbc6bf7f802c5b3c2fbf1
vrxmike <alvinvrxmike@ivloud.com>
2022-08-22 14:40:49 -0700

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
  *
- * Return: actaul number of letters it could read and print
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(size of(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
