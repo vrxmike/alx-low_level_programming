f5e3b76a5cec6568c3c34aeb6a10389ae12883fa
vrxmike <alvinvrxmike@icloud.com>
2022-09-29 21:27:54 -0700

    8-Task-hotfix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 	current = *head;
-	if (inidex == 0)
+	if (index == 0)
 	{
 		*head = current->next;
 		if (current->next != NULL)
