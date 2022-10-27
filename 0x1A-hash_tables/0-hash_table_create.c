3827ba0037294108ce532a83123740ff8ac799d4
vrxmike <alvinvrxmike@icloud.com>
2022-10-27 22:16:12 +0000

    0-Task-hotfix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,16 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
-		return NULL;
+		return (NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 
-	if(hash_table->array == NULL)
+	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-	for(i = 0;i < size; i++)
-		hash_table->array[i] = NULL);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 	return (hash_table);
 }
