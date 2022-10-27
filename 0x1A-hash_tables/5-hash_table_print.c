86adbed1fa53e984531374e2939805125f567f20
vrxmike <alvinvrxmike@icloud.com>
2022-10-27 23:32:22 +0000

    5-Task

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ *
+ * return: void
+ */
+void hash_table_print(const hash_table_print(const hash_table_t *ht)
+{
+
+}
