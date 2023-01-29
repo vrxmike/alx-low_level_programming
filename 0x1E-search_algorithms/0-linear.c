33dba213a2a0c94be36e754a6c3f8e7b5231367f
vrxmike <alvinvrxmike@icloud.com>
2023-01-29 15:00:24 -0800

    0-Task-fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,21 +4,24 @@
  * @array: Pointer to first element of array
  * @size: Number of elements in array
  * @value: Value being searched for in the array
- * Return: Index of the value found
+ * Return: Index of the value found, otherwise -1
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i;
+	size_t i;
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	for (i = 0; (i < size) && (array); i++)
 	{
-		printf("Value checked array[&d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
 			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		}
 	}
 	return (-1);
 }
